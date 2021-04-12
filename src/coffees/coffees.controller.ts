673cd7232315f518f0dcd6fcd1a2c737cb76dda6
Juan Pablo Rocha <jpr.arango10@uniandes.edu.co>
2021-04-12 10:29:15 -0500

    Hasta entities

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+
+@Controller('coffees')
+export class CoffeesController {
+constructor(private readonly coffeesService: CoffeesService) {
+  
+}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+      const { limit, offset } = paginationQuery;
+    return `This return all the coffees. Limit ${limit}, offset ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This return #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} cofee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} cofee`;
+  }
+}
